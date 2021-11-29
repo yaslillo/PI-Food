c91acb37b8994865e2ba17be11b03c70be10545a
yaslillo <yasnalillo27@gmail.com>
2021-11-29 19:35:54 -0300

    changes

--- client/src/React/components/Home.jsx
+++ client/src/React/components/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
-import {getRecipes,setRecipeOrder,filterCreated} from '../../redux/Action/index';
+import {getRecipes} from '../../redux/Action/index';
 import {Link} from 'react-router-dom';
 import Card from './Card';
 import Paginado from './Paginado';
@@ -11,16 +11,19 @@ export default function Home (){
 const dispatch = useDispatch()
 const allRecipes = useSelector((state) => state.recipes)
         console.log(allRecipes)
-//Paginado.
+//Paginado.-----------------------------------------------------------------------//
 const [currentPage,setCurrentPage] = useState(1);
 const[recipesPerPage,setRecipesPerPage] =useState(9);
 const indexOfLastRecipes = currentPage * recipesPerPage;
 const indexOfFirstRecipes = indexOfLastRecipes - recipesPerPage;
 const currentRecipes = allRecipes.slice(indexOfFirstRecipes,indexOfLastRecipes);
 
+
 const paginado = (pageNumber) => {
     setCurrentPage(pageNumber)
 }
+//---------------------------------------------------------------------------------//
+
 useEffect(() =>{
     dispatch(getRecipes());
 },[dispatch])
@@ -30,37 +33,46 @@ function handlerClick(e){
     dispatch(getRecipes());
 }
 
+
+
     return(
         <div>
             <Link to='/recipes'>Crear Receta</Link>
             <h1>LAS MEJORES RECETAS</h1> 
-            <button onClick={e=>{handlerClick(e)}}>
+            <button onClick={e => { handlerClick (e)}}>
                 volver a cargar recetas
-            </button>
+                </button>
             <div>
                 <select>
-                    <option value= "see-all">Todos</option>
-                    <option value= "score-asc">Ptje Asc</option>
-                    <option value= "score-desc">Ptje Desc</option>
-                    <option value= "alph-asc">A-Z</option>
-                    <option value= "alph-desc">Z-A</option>
+                    <option value="asc">Ascendente</option>
+                    <option value="desc">Descendente</option>
+                </select>
+                <select
+                >
+                    <option value= "All">Todos</option>
+                    <option value="dairy free">dairy free</option>
+                    <option value= "fodmap friendly">fodmap friendly</option>
+                    <option value="gluten free">gluten free</option>
+                    <option value="lacto ovo vegetarian">lacto ovo vegetarian</option>
+                    <option value= "paleolithic">paleolithic</option>
+                    <option value= "primal">primal</option>
+                    <option value="vegan">vegan</option>
                 </select>
                 <select>
                     <option value="All">Todos</option>
                     <option value="diet">Tipo de dieta</option>
                 </select>
+                    
                 <select>
                     <option value="All">Todos</option>
                     <option value="Created">Creados</option>
                     <option value="api">Existentes</option>
                 </select>
-                <p> Aca va el paginado</p>
                 <Paginado
                 recipesPerPage= {recipesPerPage}
                 allRecipes={ allRecipes.length}
                 paginado={paginado}
                 />
-                <p> Aca va el paginado</p>
                 { currentRecipes?.map((el) => {
                     return(
                         <div>
--- client/src/redux/Action/index.js
+++ client/src/redux/Action/index.js
@@ -1,109 +1,31 @@
 import axios from "axios";
 
-// export const FILTER_BY_DIET = "FILTER_BY_DIET";
 
 export function getRecipes() {
     return async function (dispatch) {
-        try {
-            let json = await axios.get('http://localhost:3001/recipes');
-            dispatch({
+    
+            let json = await axios.get('http://localhost:3001/recipes',{
+    });    
+            return dispatch({
                 type: 'GET_RECIPES',
                 payload: json.data
 
             })
 
-        } catch (err) {
-            console.log(err)
+
         }
 
     }
-}
 
-export function getRecipeDetail(id) {
-    return async function (dispatch) {
-        var json = await axios.get(`http://localhost:3001/recipes/${id}`);
-        return dispatch({
-            type: "GET_RECIPE_DETAIL",
-            payload: json.data
-        })
-    }
-}
 
-export function addRecipe(recipe) {
-    return async function (dispatch) {
-        if (!recipe) {
-            return dispatch({
-                type: "ADD_RECIPE",
-                payload: { default: true }
-            })
-        }
-        var res = await axios.post(`http://localhost:3001/recipes`, recipe);
-        return dispatch({
-            type: "ADD_RECIPE",
-            payload: res.data
-        })
-    }
-}
 export function getDiets() {
-    return async function (dispatch) {
-        var diets = await axios.get(`http://localhost:3001/types`);
-        return dispatch({
-            type: "GET_DIETS",
-            payload: diets.data
-        })
-    }
-}
-export function postRecipe(payload) {
-    console.log(payload.diets + 'Soy payload')
-    return async function (dispatch) {
-        const response = await axios.post('http://localhost:3001/recipe', payload)
-        return response
-    }
-}
-export function filterRecipesByStatus(payload) {
-    return {
-        type: 'FILTER_BY_STATUS',
-        payload
-    }
-}
-
-export function orderByName(payload) {
-    return {
-        type: "ORDER_MY_NAME",
-        payload
-    }
-}
-export function setRecipeOrder(order) {
-    return {
-        type: "SET_ORDER",
-        payload: order
-    }
-}
-export function filterByDiet(diets) {
-    return {
-        type: "FILTER_BY_DIET",
-        payload: diets
-    }
-}
-export function getNameRecipes(name) {
-    return async function (dispatch) {
-        try {
-            let json = await axios.get('http://localhost:3001/recipe?name=' + name);
-            dispatch({
-                type: 'GET_NAME_RECIPES',
-                payload: json.data
+        return async function (dispatch) {
+            const json = await axios('http://localhost:3001/diets',{
 
             })
-
-        } catch (err) {
-            console.log(err)
+            return dispatch({
+                type: 'GET_DIETS',
+                payload : json.data
+            })
         }
-
-    }
-}
-export function filterCreated(payload) {
-    return {
-        type: 'FILTER_CREATED',
-        payload
-    }
 }
